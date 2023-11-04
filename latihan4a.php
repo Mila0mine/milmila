ba97162a46646b16afb47b74f718ba445b12af7e
Mila0mine <148618076+Mila0mine@users.noreply.github.com>
2023-11-04 20:08:20 +0700

    Add files via upload

---
+++ latihan4a.php
@@ -0,0 +1,9 @@
+<?php
+
+$warna_balon = array("hijau", "kuning", "kelabu", "merah muda");
+
+
+echo "Balonku ada lima.<br>";
+echo "Rupa-rupa warna-nnya  <br>". $warna_balon[0] . ", " . $warna_balon[1] . ", " . $warna_balon[2] . ", " . $warna_balon[3] . ", dan biru <br> Meletus balon " . $warna_balon[0] . " DOR!!!<br>";
+echo "Hatiku sangat kacau..";
+?>
